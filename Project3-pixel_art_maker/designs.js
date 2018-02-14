905f7493e44e9fbeef9934e98fe274dc97ee8ae9
Julia Fuhrman <JulNicole@gmail.com>
2018-02-14 16:27:02 -0500

    Add ability to change cell css on click

--- Project3-pixel_art_maker/designs.js
+++ Project3-pixel_art_maker/designs.js
@@ -1,7 +1,9 @@
+//define default values in case User hits submit without changes
 let gridHeight = $("#inputHeight").val()
 let gridWidth = $("#inputWidth").val()
 let color = $("#colorPicker").val()
 
+//set user defined values for height, width, and color
 $("#inputHeight").change(function() {
   gridHeight = $(this).val();
 });
@@ -14,23 +16,33 @@ $("#colorPicker").change(function() {
   color = $(this).val();
 });
 
-// When size is submitted by the user, call makeGrid()
+// When form is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function(event){
-	//clear grid each time user selects new sizes
+	//clear grid when user re-submits form with new values
 	$("tbody").remove();
 
 	let rows = "";
 	let cols = "";
 
+	//set width of row
 	for(let w=0; w<gridWidth; w++){
 		cols += "<td></td>";
 	}
 
+	//set height of columns
 	for(let h=0; h<gridHeight; h++){
 		rows += "<tr>" + cols + "</tr>";
 	}
 
+	//add grid to DOM
 	$("#pixelCanvas").append(rows);
-	event.preventDefault();
-});
 
+	//change background color of each clicked cell
+	$('td').click(function(event){
+		$(event.target).css("background-color", color);
+	})
+
+	//prevent form from automatically refreshing page
+	event.preventDefault();
+
+});
