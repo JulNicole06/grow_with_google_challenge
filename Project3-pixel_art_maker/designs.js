b31ef76254950380de123837ce63a6a80b0c8766
Julia Fuhrman <JulNicole@gmail.com>
2018-02-14 15:43:16 -0500

    Add ability to draw pixel canvas

--- Project3-pixel_art_maker/designs.js
+++ Project3-pixel_art_maker/designs.js
@@ -1,10 +1,31 @@
 // Select color input
-// Select size input
+let gridHeight = $("#inputHeight").val()
+let gridWidth = $("#inputWidth").val()
+
+
+$("#inputHeight").change(function() {
+  gridHeight = $(this).val();
+});
+
+$("#inputWidth").change(function() {
+  gridWidth = $(this).val();
+});
+
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event){
+	let rows = "";
+	let cols= "";
 
-function makeGrid() {
+	//determine length of one row
+	for(let w=0; w<gridWidth; w++){
+		cols += "<td></td>";
+	}
 
-// Your code goes here!
+	for(let h=0; h < gridHeight; h++){
+		rows += "<tr>" + cols + "</tr>";
+	}
 
-}
+	$("#pixelCanvas").append(rows);
+	event.preventDefault();
+});
