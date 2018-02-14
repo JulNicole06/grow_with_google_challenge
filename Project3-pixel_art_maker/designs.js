9183f4d368df3676128dbc90ec272f40d4612b4f
Julia Fuhrman <JulNicole@gmail.com>
2018-02-14 15:54:25 -0500

    Fix: Clear grid at beginning of submit

--- Project3-pixel_art_maker/designs.js
+++ Project3-pixel_art_maker/designs.js
@@ -1,7 +1,6 @@
-// Select color input
 let gridHeight = $("#inputHeight").val()
 let gridWidth = $("#inputWidth").val()
-
+let color = $("#colorPicker").val()
 
 $("#inputHeight").change(function() {
   gridHeight = $(this).val();
@@ -11,21 +10,27 @@ $("#inputWidth").change(function() {
   gridWidth = $(this).val();
 });
 
+$("#colorPicker").change(function() {
+  color = $(this).val();
+});
 
 // When size is submitted by the user, call makeGrid()
 $('#sizePicker').submit(function(event){
-	let rows = "";
-	let cols= "";
+	//clear grid each time user selects new sizes
+	$("tbody").remove();
 
-	//determine length of one row
+	let rows = "";
+	let cols = "";
+
 	for(let w=0; w<gridWidth; w++){
 		cols += "<td></td>";
 	}
 
-	for(let h=0; h < gridHeight; h++){
+	for(let h=0; h<gridHeight; h++){
 		rows += "<tr>" + cols + "</tr>";
 	}
 
 	$("#pixelCanvas").append(rows);
 	event.preventDefault();
 });
+
